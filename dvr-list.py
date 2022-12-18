bc5b3a8d9592554aa4a39f273c4cf9a76ebb3bc8
Martin Weinzierl <35155124+Gumbini@users.noreply.github.com>
2022-12-18 02:35:55 +0100

    Multiple improvements

--- dvr-list.py
+++ dvr-list.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import cv2
 from datetime import datetime
 import re as regex
 import sys
@@ -7,45 +8,57 @@ import sys
 recordings = []
 
 class Recording:
-    def __init__(self, meta):
-            self.channel     = meta[0].split(":")[-1].strip()
-            self.sortkey     = alphanumeric(meta[1]).lower()
-            self.title       = meta[1].strip()
-            self.description = remove_prefix(meta[2].strip(), self.title)[:24].strip()
-            self.timestamp   = datetime.fromtimestamp(int(meta[3]))
-            self.length      = int(int(meta[5].strip()) / 90_000)
-            self.hd          = "hd" in self.channel.lower()
-
+    def __init__(self, path: str, meta: str):
+        self.path        = path.strip()
+        self.channel     = meta[0].split(":")[-1].strip()
+        self.sortkey     = alphanumeric(meta[1]).lower()
+        self.title       = meta[1].strip()
+        self.description = remove_prefix(meta[2].strip(), self.title).strip()
+        self.timestamp   = datetime.fromtimestamp(int(meta[3]))
+        self.length      = int(int(meta[5].strip()) // 90_000)
+        self.hd          = "hd" in self.channel.lower()
+#       self.resolution  = get_video_metadata(path)
 
     def __repr__(self):
-        return f"{format_length(self.length)} | {'HD' if self.hd else '  '} | {self.title} | {self.description}"
+        return f"{format_length(self.length)} | {self.channel[:8].ljust(8)} | {self.title[:43].ljust(43)} | {self.description[:73]}"
 
 def alphanumeric(line: str) -> str:
     return regex.sub("[^A-Za-z0-9]+", "", line)
 
-def format_length(seconds) -> str:
-    minutes = seconds / 60
-    return f"{int(minutes / 60)}h {int(minutes % 60):02d}'"
+def format_length(raw: int) -> str:
+    hours   = raw // 3_600
+    raw -= hours * 3_600
+    minutes = raw // 60
+    raw -= minutes * 60
+    seconds = raw
+    return f"{hours}:{minutes:02d}:{seconds:02d}"
 
 def remove_prefix(line: str, prefix: str):
     return regex.sub(r'^{0}'.format(regex.escape(prefix)), '', line)
 
+def get_video_metadata(path: str):
+    vid     = cv2.VideoCapture(path)
+#   fps     = int(vid.get(cv2.CAP_PROP_FPS))
+    quality = int(vid.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    return f"{quality}{'p' if quality == 720 else 'i'}".rjust(5)
+
 def main(argc: int, argv: list[str]) -> None:
-    print("Waiting for meta files or EOF")
+    print("Waiting for *.ts file paths or EOF")
     while True:
         try:
-            with open(input()) as f:
-                recordings.append(Recording(f.readlines()))
+            path = input()
+            with open(path + ".meta") as file:
+                recordings.append(Recording(path, file.readlines()))
+        except FileNotFoundError:
+            continue
         except EOFError:
-            print("EOF received")
             break
 
-    print("Begin sorting...")
     recordings.sort(key=lambda r: r.sortkey)
-    print("Sorting finished")
 
-    for r in recordings:
-        print(r)
+    padding = len(str(len(recordings)))
+    for i, r in enumerate(recordings):
+        print(f"{str(i).rjust(padding)} | {r}")
 
 if __name__ == "__main__":
     main(len(sys.argv), sys.argv)
